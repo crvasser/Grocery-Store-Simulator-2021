0c55b40d164b7a876177a79815758064e31870e7
crvasser <crvasser@mtu.edu>
2020-02-17 13:48:11 -0500

    Initial setup commit

---
+++ money.py
@@ -0,0 +1,6 @@
+totalMoney = 0
+# need init function, sets totalMoney
+
+# need get money function
+
+# need set money function
---
+++ supplier.py
@@ -0,0 +1,5 @@
+# global inventory array
+
+# need init function, innitialize class and inventory array
+
+# need purchase stock function, checks if stock is available and removes it from inventory array
