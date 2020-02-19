c3da666bd8444c3291d84d5caa70befa7c820236
advandri <adgvandrie12@gmail.com>
2020-02-19 13:26:08 -0500

    Added init method and get/set functionality to the money class.

--- money.py
+++ money.py
@@ -1,6 +1,13 @@
-totalMoney = 0
-# need init function, sets totalMoney
 
-# need get money function
-
-# need set money function
+class money:
+    amount = 0
+    # need init function, sets totalMoney
+    def init(self, amount):
+        self.amount = amount
+        return self
+    # need get money function
+    def get_money(self):
+        return self.amount
+    # need set money function
+    def set_money(input):
+        amount = input
