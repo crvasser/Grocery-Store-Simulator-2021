a8ebe54178cbb3ceee654a60292f27cc1047d01b
crvasser <crvasser@mtu.edu>
2020-02-19 13:08:03 -0500

    Initial setup commit

--- user.py
+++ user.py
@@ -1,1 +1,12 @@
-input
+
+string = input("type start to begin game, type quit to quit")
+if "quit" in string:
+    quit()
+if "start" in string:
+    print("game has begun")
+    supplier = supplier.init()
+    store = store.init()
+    while():
+        string = input("type product (amount) to add item to stock")
+        if string[0] in supplier.getProduce():
+            store.add(string[0])
