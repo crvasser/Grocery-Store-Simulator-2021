7866311c5eabd46de8bf5234f217b6049442790b
Paul Mahowald <pjmahowa@mtu.edu>
2020-02-21 13:30:51 -0500

    Supplier methods work and return correctly

--- supplier.py
+++ supplier.py
@@ -21,22 +21,30 @@ class supplier:
     # takes in item name and amount requested
     #
     def purchaseStock(self, item, amount):
-        if self.stockAvailable(self, item, amount):
-            return self.removeFromInventory(self, item, amount)
-
-    # checks if the item can be purchased in that amount
-    def stockAvailable(self, item, amount):
-        if item in self.stock:
-            if self.stock[self.stock.index(item)][1] >= amount:
+        if self.stockAvailable(item, amount):
+            if self.removeFromInventory(item, amount):
                 return True
             else:
                 return False
         else:
             return False
 
+    # checks if the item can be purchased in that amount
+    def stockAvailable(self, item, amount):
+        for i in self.stock:
+            if item in i[0]:
+                if i[1] >= amount:
+                    return True
+                else:
+                    return False
+
     # removes from inventory and returns true.
     def removeFromInventory(self, item, amount):
-        return
+        for i in self.stock:
+            if item in i[0]:
+                i[1] = i[1] - amount
+                return True
+        return False
 
     # returns the price of the item from seller
     def sellerItemPrice(self, item):
--- supplierTest.py
+++ supplierTest.py
@@ -2,7 +2,13 @@ from supplier import supplier
 import unittest
 
 supplier = supplier()
-print(supplier.sellerItemPrice("carrot"))
+print(supplier.sellerItemPrice("Carrots"))
+print(supplier.stockAvailable("Carrots", 10))
+print(supplier.removeFromInventory("Carrots", 10))
+print(supplier.purchaseStock("Carrots", 10))
+
+
+
 class supplierTest:
     def test(self):
         self.assertEqual(supplier.sellerItemPrice("carrot"), 0.57)
