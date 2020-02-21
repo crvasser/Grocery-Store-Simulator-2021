b5e6b2a4270769b0ac316589ae24da7f41b4dce9
andrewhegg <ahegg@mtu.edu>
2020-02-20 20:35:32 -0500

    updated add and checkInventory functions and added comments

--- store1.py
+++ store1.py
@@ -1,22 +1,37 @@
+from money import money
+# might not need this if we decided to check the money in the supplier purchaseStock method
+
+
 class store:
     inventory = [[], [], []]
+# stored like [PRODUCT, STOCK, PRICE]
 
     def __init__(self):
         self.inventory = store.inventory
 
-    def add(product, amount, supplier, buyPrice):
-        if supplier.purchaseStock(product, amount, buyPrice):
-            checkInventory(product, amount)
-        else:
+# should first check that there is enough money, then if supplier has enough product
+# then call to checkInventory to handle adding to store's stock
+    def add(self, product, amount, supplier, buyPrice):
+        if money.getMoney() < buyPrice * amount:
+            print("Cannot afford " + amount + " " + product + "\n")
+            return
+        if supplier.stockAvailable(product, amount):
             sellPrice = supplier.getPrice(product)
-            inventory.append([product, amount, sellPrice])
+            self.checkInventory(product, amount, sellPrice)
 
-    def checkInventory(product, amount):
-        for i in inventory:
+            # not sure if we are updating the money here or in supplier
+            money.setMoney(money.getMoney()-(buyPrice*amount))
+        else:
+            print(amount + " " + product + " not in stock\n")
+
+# checks if there is an entry in the store's stock for the given product then either creates a new entry
+# or updates the existing entry's amount value
+    def checkInventory(self, product, amount, sellPrice):
+        for i in self.inventory:
             if product in i[0]:
-                i[0][1] = i[0][1] + amount
+                i[1] = i[1] + amount
                 return
+        self.inventory.append([product, amount, sellPrice])
 
 # need init function, init store init inventory array
 
-# need add stock function, checks if supplier has stock, and if you can afford init
