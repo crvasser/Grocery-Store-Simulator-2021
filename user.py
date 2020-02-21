05f546960746aeefa7d8514d5ba52c7ac674b727
advandri <adgvandrie12@gmail.com>
2020-02-21 13:30:54 -0500

    Changed syntax to python 3

--- user.py
+++ user.py
@@ -1,16 +1,16 @@
 from money import money
-from supplier import Supplier
-from store1 import store
+from supplier import supplier
+from store import store
 
-string = raw_input("type start to begin game, type quit to quit")
+string = input("type start to begin game, type quit to quit")
 if 'quit' in string:
     quit()
 if 'start' in string:
     print("game has begun")
     money = money(2000)
-    supplier = Supplier()
-    store = store.init()
-    while ():
+    supplier = supplier()
+    store = store()
+    while (1):
         string = input("type product (amount) to add item to stock")
         if string[0] in supplier.getProduce():
             if isinstance(string[1], 'int'):
