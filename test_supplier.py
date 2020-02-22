28a763560f62439789751441a40a1681e98e21cd
Paul Mahowald <pjmahowa@mtu.edu>
2020-02-21 21:44:19 -0500

    made a test file for supplier, will put in more tests later but for now got it working

---
+++ test_supplier.py
@@ -0,0 +1,20 @@
+import unittest
+from supplier import supplier
+
+class TestSupplier(unittest.TestCase):
+    def setUp(self):
+        self.supplier = supplier()
+
+class TestPurchase(TestSupplier):
+
+    def test_purchaseStock(self):
+        self.assertEqual(self.supplier.sellerItemPrice("Carrots"), 0.56)
+
+    def test_stockAvailable(self):
+        self.assertTrue(self.supplier.stockAvailable("Carrots", 10))
+
+    def test_removeFromInventory(self):
+        self.assertTrue(self.supplier.removeFromInventory("Carrots", 10))
+
+    def test_sellerItemPrice(self):
+        self.assertTrue(self.supplier.sellerItemPrice("Carrots"))
