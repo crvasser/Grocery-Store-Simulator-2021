c92763b53c15f677e4f4ff5505e2b14d1ed25176
crvasser <crvasser@mtu.edu>
2020-04-15 13:20:41 -0400

    no longer remakes menu on event text

--- GUIuser.py
+++ GUIuser.py
@@ -444,10 +444,11 @@ while playing_game:
         curTime2 = pygame.time.get_ticks()
         text = customer.supplierRandomPriceChange(supplier)
         eventText = myfont.render("{0}".format(text), 1, (0, 0, 0))
-        screen.fill(white, (0, 0, screen.get_width(), screen.get_height() // 20))
+        screen.fill(white, (0, 0, screen.get_width(), 100))
         storeUpdate = 1
         supplierUpdate = 1
         screen.blit(eventText, (5, 60))
+
     if shoplifterTime + 22000 < pygame.time.get_ticks() and startShopLifting == 0:  # change time added to shoplifting time for balance keep the 2k offset to avoid overlap
         firstShoplifter = 1
         shoplifterTime = pygame.time.get_ticks()
